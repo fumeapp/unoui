5652683744cdee6412a0871c7b664dfee5c9733f
kevin olson <acidjazz@gmail.com>
2023-06-19 20:47:33 -0500

    ✨ working icons

--- playground/app.vue
+++ playground/app.vue
@@ -1,8 +1,8 @@
 <template>
   <div flex flex-col space-y-4 items-center justify-center w-screen h-screen bg-gray-900>
-    <u-button icon="mdi:ab-testing" size="xl">u-button</u-button>
+    <u-button icon="i-mdi:ab-testing" size="xl">u-button</u-button>
     <span text-gray-100>icon below</span>
-    <div class="mdi-ab-testing w-24 h-24 border border-red-500 text-white bg-red-100" />
+    <div i-mdi-grid w-24 h-24 border border-red-500 text-white bg-red-100 />
     <span text-gray-100>icon above</span>
   </div>
 </template>
